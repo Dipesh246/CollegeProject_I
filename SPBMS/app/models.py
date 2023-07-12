cba3ff6f950f703fbcba9889afa6001cb2d871fe
Dipesh <dipeshbist760@gmail.com>
2023-07-12 12:31:28 +0545

    made models

---
+++ SPBMS/app/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser,Group, Permission
+
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_picture/', blank=True)
+    registration_date = models.DateField(auto_now_add=True)
+    groups = models.ManyToManyField(Group, related_name='app_users')
+    user_permissions = models.ManyToManyField(Permission, related_name='app_users')
+
+    def __str__(self) -> str:
+        return self.username
+    
+class Budeget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    budget_name = models.CharField(max_length=100)
+    total_budget_amount = models.DecimalField(max_digits=10,decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.budget_name
+    
+class Category(models.Model):
+    budget = models.ForeignKey(Budeget,on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.category_name
+    
+class Expense(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    budget = models.ForeignKey(Budeget, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    expense_name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10,decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.expense_name
+    
+class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    goal_name = models.CharField(max_length=100)
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.goal_name
+    
+class Saving(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    saving_name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.saving_name
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    TYPE_CHOICES = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.type} - {self.amount}'
+    
+# class Reminder(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=100)
+#     description = models.TextField()
+#     date = models.DateField()
+#     time = models.TimeField()
+
+#     def __str__(self):
+#         return self.title
+
